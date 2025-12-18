736b6e5b4c5a9455586e500cbb2586f5678b34d9
TS <ashlokkumar2006@gmail.com>
2025-12-18 22:28:56 +0530

    Fix scanf format string for two integers

--- Q.10.c
+++ Q.10.c
@@ -5,11 +5,12 @@ int main()
 
     int a = 0, b = 0;
     printf("Enter two different integer numbers:\n");
-    scanf("%d %d %d", &a, &b);
+    scanf("%d %d", &a, &b);
 
     // using the condition operator
     // ( condition )? true block : false block ;
     (a > b) ? printf("a is greater than b\n") : printf("b is greater than a\n");
 
     return 0;
+
 }
